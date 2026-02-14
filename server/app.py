6d73291afcdc6734b356a873887a9abe295efbb3
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-02-14 14:55:30 +1100

    updated table columns and relationships

--- server/app.py
+++ server/app.py
@@ -13,17 +13,21 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/')
 def home():
     return '<h1>Zoo app</h1>'
+
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
     return ''
 
+
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
     return ''
+
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
